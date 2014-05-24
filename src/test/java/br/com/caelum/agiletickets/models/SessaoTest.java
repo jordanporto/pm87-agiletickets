4994123c39a376f3ab20f8e5a1203e86dff2e434
jordan <jordan.porto@credsystem.com.br>
2014-05-24 10:33:12 -0300

    consertando bugs

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -14,6 +14,15 @@ public class SessaoTest {
 	}
 	
 	@Test
+	public void deveVender21ingressoSeHa2vagas() throws Exception {
+		Sessao sessao = new Sessao();
+        sessao.setTotalIngressos(2);
+
+        Assert.assertTrue(sessao.podeReservar(2));
+	}
+	
+	
+	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
